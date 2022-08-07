64fdce52700992d9733f70b38ef56695a0e16a2f
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-08-07 12:47:52 +0530

    Time: 12 ms (70.34%), Space: 57 MB (30.75%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs==null || strs.length==0){
+            return new ArrayList<>();
+        }
+       HashMap<String,List<String>>  map = new HashMap<>();
+        for(String s: strs){
+            char[] ch =new char[26];
+            for(char c :s.toCharArray()){
+                ch[c-'a']++;
+            }
+            String keypair=String.valueOf(ch);
+            if(!map.containsKey(keypair)){
+                map.put(keypair,new ArrayList<>());
+                
+            }
+            map.get(keypair).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
