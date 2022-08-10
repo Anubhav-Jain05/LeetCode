a4d6e907d83fa3d535f5b5472177c30630f6b2df
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-08-11 02:12:42 +0530

    Time: 1 ms (78.58%), Space: 51.8 MB (12.95%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,18 +1,31 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        /*int arr[]= new int[nums.length];
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]+1!=nums[i+1]){
-                arr[i]=i;
+        //int arr[]= new int[nums.length];
+       // Arrays.sort(nums);
+       /* int sum=nums.length;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i+1]!=nums[i]+1){
+                sum += nums[i];;
             }
         }
-        return arr;*/
+        return sum;*/
         
-        int sum=nums.length;
+       /* int sum=nums.length;
+        //System.out.print(sum);
         for(int i=0;i<nums.length;i++){
             sum += i-nums[i];
+            System.out.print(sum);
+           System.out.print(i+" ");
+            System.out.print(nums[i]);
         }
-        return sum;
+       // System.out.print(sum);
+        return sum;*/
+        
+        int xor=0;
+        int i=0;
+        for(i=0;i<nums.length;i++){
+            xor=xor^i^nums[i];
+        }
+        return xor^i;
     }
 }
