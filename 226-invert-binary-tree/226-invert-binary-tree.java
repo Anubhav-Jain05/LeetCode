b3514b1968ad23cdff8fa0385de485779e3716eb
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-08-17 16:38:12 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (42.42%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        Queue<TreeNode> q= new LinkedList<>();
+       /* Queue<TreeNode> q= new LinkedList<>();
         if(root==null){
             return root;
         }
@@ -37,6 +37,14 @@ class Solution {
                 }
             }
         }
+        return root;*/
+        
+        if(root==null){
+            return root;
+        }
+        TreeNode temp= root.left;
+        root.left=invertTree(root.right);
+        root.right=invertTree(temp);
         return root;
     }
 }
