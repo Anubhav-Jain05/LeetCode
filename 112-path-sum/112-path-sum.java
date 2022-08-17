945077179160457241812b02da62260181c13979
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-08-17 11:42:14 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (46.35%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -52,7 +52,7 @@ class Solution {
         if(root==null){
             return false;
         }
-        if(root.left==null && root.right==null && root.val==targetSum){
+       if(root.left==null && root.right==null && root.val==targetSum){
             return true;
         }
         return hasPathSum(root.left,targetSum-root.val)  || hasPathSum(root.right,targetSum-root.val);
