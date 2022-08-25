d55655844df78c3f5bc6d4c42455fe246324155e
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-08-25 20:51:19 +0530

    Time: 5 ms (74.47%), Space: 45.4 MB (81.66%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> map = new HashMap<>();
+       /* HashMap<Character,Integer> map = new HashMap<>();
         for(char ch : magazine.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
@@ -11,6 +11,18 @@ class Solution {
                 return false;
             }
         }
+        return true;*/
+        
+        int[] arr= new int[26];
+        for(int i=0;i<magazine.length();i++){
+            arr[magazine.charAt(i)-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            if(--arr[ransomNote.charAt(i)-'a']<0){
+                return false;
+            }
+        }
         return true;
+        
     }
 }
