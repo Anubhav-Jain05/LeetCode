2f9c6884d1d4ebb84e41bb6c78f3bd7c2a1d655c
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-08-29 12:33:43 +0530

    Time: 1378 ms (14.03%), Space: 145.1 MB (5.12%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int threeSumMulti(int[] arr, int target) {
+       /* int count=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                for(int k=j+1;k<arr.length;k++){
+                    if(arr[i]+arr[j]+arr[k]==target){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;*/
+       
+       Map<Integer, Integer> map = new HashMap<>();
+        
+        int res = 0;
+        int mod = 1000000007;
+        for (int i = 0; i < arr.length; i++) {
+            res = (res + map.getOrDefault(target - arr[i], 0)) % mod;
+            
+            for (int j = 0; j < i; j++) {
+                int temp = arr[i] + arr[j];
+                map.put(temp, map.getOrDefault(temp, 0) + 1);
+            }
+        }
+        return res;
+    }
+}
