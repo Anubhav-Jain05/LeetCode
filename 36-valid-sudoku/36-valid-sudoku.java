a62cde09dcc8f290d86d18251eccdfff10faf8dd
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-08-29 19:12:59 +0530

    Time: 15 ms (38.37%), Space: 42.8 MB (89.76%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> hs =new HashSet<>();
+       /* HashSet<String> hs =new HashSet<>();
        // Set hs = new HashSet();
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
@@ -12,6 +12,60 @@ class Solution {
                 }
             }
         }
-        return true;
+        return true;*/
+        
+        //HashSet<Integer> hs = new HashSet<>();
+       /* Set hs = new HashSet();
+        for(int i=0;i<9;++i){
+            for(int j=0;j<9;++j){
+                char no= board[i][j];
+                if(no != '.'){
+                    if(!hs.add(no + i) || 
+                       !hs.add(no + j) || 
+                       !hs.add( no + i/3 +"-"+ j/3)){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;*/
+        
+        Set seen = new HashSet();
+    for (int i=0; i<9; ++i) {
+        for (int j=0; j<9; ++j) {
+            char number = board[i][j];
+            if (number != '.')
+                if (!seen.add(number + " in row " + i) ||
+                    !seen.add(number + " in column " + j) ||
+                    !seen.add(number + " in block " + i/3 + "-" + j/3))
+                    return false;
+        }
+    }
+    return true;
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
