438fead958bb54724e9c5d0031c55e8ee1f82abc
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-08-30 20:33:52 +0530

    Time: 1 ms (38.11%), Space: 43 MB (28.88%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-         /* for(int i=0;i<matrix.length;i++){
+         for(int i=0;i<matrix.length;i++){
               for(int j=0;j<matrix[0].length;j++){
                   if(matrix[i][j]==target){
                       return true;
                   }
               }
           }
-        return false;*/
+        return false;
         
         
-        int n=matrix.length;
+        /*int n=matrix.length;
         int m=matrix[0].length;
         
         int i=0;
@@ -28,6 +28,6 @@ class Solution {
             }
             
         }
-        return false;
+        return false;*/
     }
 }
