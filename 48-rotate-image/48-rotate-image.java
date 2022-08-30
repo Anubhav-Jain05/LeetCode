40fcc8b69811599bcb69dedec8dcb331913c6377
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-08-30 18:25:32 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (14.29%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,5 +1,8 @@
 class Solution {
     public void rotate(int[][] matrix) {
+        
+        // first transpose the matrix    1,2,3==1,4,7
+        
         for(int i=0;i<matrix.length;i++){
             for(int j=i;j<matrix[0].length;j++){
                 int temp=0;
@@ -8,6 +11,8 @@ class Solution {
                 matrix[j][i]=temp;
             }
         }
+        
+        // second step- reverse the matrix    1,4,7==7,4,1
         
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length/2;j++){
