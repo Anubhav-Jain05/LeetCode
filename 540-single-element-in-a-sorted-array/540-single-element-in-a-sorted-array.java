2a3b97d60390b5d6f2534b9e17e820a6cd8e31da
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-09-04 17:07:29 +0530

    Time: 0 ms (100.00%), Space: 61.2 MB (9.82%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,8 +1,11 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-       /* for(int i=0;i<nums.length;i++){
+        
+        //TLE
+        
+        /*for(int i=0;i<nums.length;i++){
             int count=0;
-            for(int j=i;j<nums.length;j++){
+            for(int j=0;j<nums.length;j++){
                 if(nums[i]==nums[j]){
                     count++;
                 }
@@ -13,7 +16,7 @@ class Solution {
         }
         return 0;*/
         
-        HashMap<Integer,Integer> map = new HashMap<>();
+        /*HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
@@ -22,6 +25,18 @@ class Solution {
                 return entry.getKey();
             }
         }
-        return 0;
+        return 0;*/
+        
+        int low=0;
+        int high=nums.length-1;
+        while(low<high){
+            int mid=(high+low)/2;
+            if((mid %2==0 && nums[mid]==nums[mid+1] )|| (mid %2==1 && nums[mid]==nums[mid-1])){
+                low=mid+1;
+            }else{
+                high=mid;
+            }
+        }
+        return nums[low];
     }
 }
