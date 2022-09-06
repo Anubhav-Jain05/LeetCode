3e1e72f0e4ef6802f58888e7e6dabf0cde59d5e2
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-09-06 15:06:00 +0530

    Time: 48 ms (46.06%), Space: 42.3 MB (94.61%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -10,32 +10,5 @@ class Solution {
             }
         }
         return result;
-        
-       /* int[] res= new int[2];
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    res[0]=i;
-                    res[1]=j;
-                }
-            }
-        }
-        return res;*/
-       /* Arrays.sort(nums);
-        int[] res=new int[2];
-        int i=0;
-        int j= nums.length-1;
-        while(i<j){
-            if(nums[i]+nums[j]<target){
-                i++;
-            }else if(nums[i]+nums[j]>target){
-                j--;
-            }else{
-                res[0]=i;
-                res[1]=j;
-                return res;
-            }
-        }
-        return res;*/
     }
 }
