04cb6882652edf0115ee068bfd4a2671298ef02e
Anubhav Jain <77787032+Anubhav-Jain05@users.noreply.github.com>
2022-09-19 13:16:27 +0530

    Time: 14 ms (7.19%), Space: 44 MB (69.07%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,12 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-       int arr[]=new int[nums1.length];
-        int n=nums1.length;
+//        int arr[]=new int[nums1.length];
+//         int n=nums1.length;
         
-        for(int i=0;i<n;i++){
-            int grea=-1, j= nums2.length-1;
-            while(j > 0 && nums1[i] != nums2[j]){
-                if(nums2[j]>nums1[i]){
+//         for(int i=0;i<n;i++){
+//             int grea=-1, j= nums2.length-1;
+//             while(j > 0 && nums1[i] != nums2[j]){
+//                 if(nums2[j]>nums1[i]){
+//                     grea=nums2[j];
+//                 }
+//                 j--;
+//             }
+//             arr[i]=grea;
+//         }
+//         return arr;
+        
+        int arr[]= new int [nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            int grea=-1,j=nums2.length-1;
+            while(j>0 && nums1[i] != nums2[j]){
+                if(nums1[i] < nums2[j]){
                     grea=nums2[j];
                 }
                 j--;
